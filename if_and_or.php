e310f65f95a3127a40084fc0c8121b8dd661c79e
Matt Valdez <mattvaldez01@gmail.com>
2017-03-13 14:47:56 -0500

    added 'if', 'and', 'or' exercise

---
+++ if_and_or.php
@@ -0,0 +1,39 @@
+<?php
+
+$x = 0;
+$y = 5;
+$z = 10;
+
+// TODO:
+if ($x < $y && $y < $z) {
+	echo "{$x} < {$y} < {$z}\n";
+}
+
+// TODO:
+if (0 < $x || $x < 10){
+	echo "0 is less than {$x} OR {$x} is less than 10" . PHP_EOL;;
+}
+
+// TODO:
+if (0 < $y || $y < 10){
+	echo "0 is less than {$y} OR {$y} is less than 10" . PHP_EOL;;
+}
+
+if (0 < $z || $z < 10){
+	echo "0 is less than {$z} OR {$z} is less than 10" . PHP_EOL;;
+}
+
+
+// TODO:
+if (0 < $x && $x < 10){
+	echo "0 is less than {$x} AND {$x} is less than 10" . PHP_EOL;;
+}
+
+// TODO:
+if (0 < $y && $y < 10){
+	echo "0 is less than {$y} AND {$y} is less than 10" . PHP_EOL;;
+}
+
+if (0 < $z && $z < 10){
+	echo "0 is less than {$z} AND {$z} is less than 10" . PHP_EOL;;
+}
