cc23fd4acf5a5dd20c3c87539c875e677844caf2
Matt Valdez <mattvaldez01@gmail.com>
2017-03-14 13:24:37 -0500

    'count by' number is set to default to 1 if user doesn't type in a number

--- for.php
+++ for.php
@@ -9,6 +9,10 @@ $endNumber = trim(fgets(STDIN));
 fwrite(STDOUT, "Pick a number to count by: ");
 $countByNumber = trim(fgets(STDIN));
 
+if (!is_numeric($countByNumber)) {
+	$countByNumber = 1;
+}
+
 for ($i = $startNumber; $i <= $endNumber; $i += $countByNumber) {
 	fwrite(STDOUT, $i . PHP_EOL);
 }
