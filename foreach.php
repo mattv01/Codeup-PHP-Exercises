d5b15a3b7795ae56a8faea593e8e5371e440ceb5
Matt Valdez <mattvaldez01@gmail.com>
2017-03-15 10:09:43 -0500

    added 'for-each loop' exercise and iterated through the array and echoed the value's type

---
+++ foreach.php
@@ -0,0 +1,19 @@
+<?php 
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+ 
+foreach ($things as $value) {
+	if (is_integer($value)) {
+		echo "{$value} is a integer" . PHP_EOL;
+	} elseif (is_float($value)) {
+		echo "{$value} is a float" . PHP_EOL;
+	} elseif (is_bool($value)) {
+		echo "{$value} is a boolean" . PHP_EOL;
+	}  elseif (is_array($value)) {
+		echo "{$value} is a array" . PHP_EOL;
+	} elseif (is_null($value)) {
+		echo "{$value} is a null" . PHP_EOL;
+	} elseif (is_string($value)) {
+		echo "{$value} is a string" . PHP_EOL;
+	}
+}
