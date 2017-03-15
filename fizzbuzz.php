b4bdbc650e656f04a649b020a61ab39330e82fbb
Matt Valdez <mattvaldez01@gmail.com>
2017-03-14 19:22:31 -0500

    added FizzBuzz exercise

---
+++ fizzbuzz.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) { 
+	if (($i % 15) == 0) {
+		fwrite(STDOUT, "FizzBuzz" . PHP_EOL);
+	} elseif (($i % 5) == 0) {
+		fwrite(STDOUT, "Buzz" . PHP_EOL);
+	} elseif (($i % 3) == 0) {
+		fwrite(STDOUT, "Fizz" . PHP_EOL);
+	} else {
+		fwrite(STDOUT, $i . PHP_EOL);
+	}
+}
