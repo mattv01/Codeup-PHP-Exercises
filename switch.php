31ffe42c98c0542273df63b43b6c13191571a766
Matt Valdez <mattvaldez01@gmail.com>
2017-03-15 12:13:59 -0500

    added switch exercise with a switch statement and if statement to check what day it is and see how the switch/if statements compare to eachother

---
+++ switch.php
@@ -0,0 +1,48 @@
+<?php
+
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$dayOfWeek = date('N');
+
+switch($dayOfWeek) {
+	case 1:
+		echo "Monday" . PHP_EOL;
+		break;
+	case 2:
+		echo "Tuesday" . PHP_EOL;
+		break;
+	case 3:
+		echo "Wednesday" . PHP_EOL;
+		break;
+	case 4:
+		echo "Thursday" . PHP_EOL;
+		break;
+	case 5:
+		echo "Friday" . PHP_EOL;
+		break;
+	case 6:
+		echo "Saturday" . PHP_EOL;
+		break;
+	case 7:
+		echo "Sunday" . PHP_EOL;
+		break;
+}
+
+
+if ($dayOfWeek == 1) {
+	echo "Monday" . PHP_EOL;
+} elseif ($dayOfWeek == 2) {
+	echo "Tuesday" . PHP_EOL;
+} elseif ($dayOfWeek == 3) {
+	echo "Wednesday" . PHP_EOL;
+} elseif ($dayOfWeek == 4) {
+	echo "Thursday" . PHP_EOL;
+} elseif ($dayOfWeek == 5) {
+	echo "Friday" . PHP_EOL;
+} elseif ($dayOfWeek == 6) {
+	echo "Saturday" . PHP_EOL;
+} elseif ($dayOfWeek == 7) {
+	echo "Sunday" . PHP_EOL;
+}
