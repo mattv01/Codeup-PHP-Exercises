2c3b9f50d8fcdf91e0f5bfd92061583be03830c4
Matt Valdez <mattvaldez01@gmail.com>
2017-03-17 15:03:39 -0500

    looking at the difference between echo and return

--- arithmetic.php
+++ arithmetic.php
@@ -1,36 +1,72 @@
 <?php
 
-$a = 5;
-$b = 10;
+// $a = 500;
+// $b = 10;
+
+// function add($a, $b)
+// {
+// 	echo "$a + $b = " . ($a + $b) . PHP_EOL;
+// }
+
+// function subtract($a, $b)
+// {
+// 	echo "$a - $b = " . ($a - $b) . PHP_EOL;
+// }
+
+// function multiply($a, $b)
+// {
+// 	echo "$a * $b = " . ($a * $b) . PHP_EOL;
+// }
+
+// function divide($a, $b)
+// {
+// 	echo "$a / $b = " . ($a / $b) . PHP_EOL;
+// }
+
+// function modulus($a, $b)
+// {
+// 	echo "$a % $b = " . ($a % $b) . PHP_EOL;
+// }
+
+
+// add($a, $b) . PHP_EOL;
+// subtract($a, $b) . PHP_EOL;
+// multiply($a, $b) . PHP_EOL;
+// divide($a, $b) . PHP_EOL;
+// modulus($a, $b) . PHP_EOL;
+
+
+
+
 
 function add($a, $b)
 {
-    echo "$a + $b = " . ($a + $b) . PHP_EOL;
+    return $a + $b;
 }
 
 function subtract($a, $b)
 {
-    echo "$a - $b = " . ($a - $b) . PHP_EOL;
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    echo "$a * $b = " . ($a * $b) . PHP_EOL;
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
-    echo "$a / $b = " . ($a / $b) . PHP_EOL;
+    return $a / $b;
 }
 
 function modulus($a, $b)
 {
-    echo "$a % $b = " . ($a % $b) . PHP_EOL;
+    return $a % $b;
 }
 
 
-add($a, $b) . PHP_EOL;
-subtract($a, $b) . PHP_EOL;
-multiply($a, $b) . PHP_EOL;
-divide($a, $b) . PHP_EOL;
-modulus($a, $b) . PHP_EOL;
+echo add(1, 5) . PHP_EOL;
+echo subtract(1, 5) . PHP_EOL;
+echo multiply(1, 5) . PHP_EOL;
+echo divide(1, 5) . PHP_EOL;
+echo modulus(1, 5) . PHP_EOL;
