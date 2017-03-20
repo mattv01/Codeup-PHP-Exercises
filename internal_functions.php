dcd7265fd7b41b9ccea391176d377fcf60dde1b6
Matt Valdez <mattvaldez01@gmail.com>
2017-03-20 11:22:45 -0500

    created function to check the type of each variable and echo the result

--- internal_functions.php
+++ internal_functions.php
@@ -2,8 +2,17 @@
 
 // TODO: Create your inspect() function here
 
-function inspect(){
-
+function inspect($inspect){
+	if (is_array($inspect)) {
+		echo " is an " . gettype($inspect) . PHP_EOL;
+	} elseif (is_bool($inspect)) {
+		echo " is a " . gettype($inspect) . PHP_EOL;
+	} elseif (is_null($inspect)) {
+		echo " is a " . gettype($inspect) . PHP_EOL;
+	}
+	else {
+		echo "'$inspect' is a " . gettype($inspect) . PHP_EOL;
+	}
 }
 
 // Do not mofify these variables!
@@ -22,23 +31,34 @@ $null = NULL;
 // TODO: After each echo statement, use inspect() to output the variable's type and its value
 
 echo 'Inspecting $num1:' . PHP_EOL;
+inspect($num1);
 
 echo 'Inspecting $num2:' . PHP_EOL;
+inspect($num1);
 
 echo 'Inspecting $num3:' . PHP_EOL;
+inspect($num3);
 
 echo 'Inspecting $num4:' . PHP_EOL;
+inspect($num4);
 
 echo 'Inspecting $null:' . PHP_EOL;
+inspect($null);
 
 echo 'Inspecting $bool1' . PHP_EOL;
+inspect($bool1);
 
 echo 'Inspecting $bool2' . PHP_EOL;
+inspect($bool2);
 
 echo 'Inspecting $string1' . PHP_EOL;
+inspect($string1);
 
 echo 'Inspecting $string2' . PHP_EOL;
+inspect($string2);
 
 echo 'Inspecting $array1' . PHP_EOL;
+inspect($array1);
 
 echo 'Inspecting $array2' . PHP_EOL;
+inspect($array2);
