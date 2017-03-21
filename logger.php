605da408b5748e94b99beb8892da83d15b620111
Matt Valdez <mattvaldez01@gmail.com>
2017-03-21 18:21:26 -0500

    added functions to display messages (info/error); took out a parameter on logMessage function to work with the other two functions

--- logger.php
+++ logger.php
@@ -1,14 +1,24 @@
 <?php
 
-function logMessage($logLevel, $message)
+function logMessage($message)
 {
 	$date = date("Y-m-d");
 	$time = date("h:i:s");
 
 	$filename = "log-$date.log";
 	$handle = fopen($filename, 'a');
-	fwrite($handle, PHP_EOL . "$date $time [$logLevel] $message");
+	fwrite($handle, PHP_EOL . "$date $time $message");
 	fclose($handle);
 }
-logMessage("INFO", "This is an info message.");
-logMessage("ERROR","This is an error message.");
+
+
+function logInfo($message){
+	logMessage($message);
+}
+logInfo("[INFO] This is an info message.");
+
+
+function logError($message){
+	logMessage($message);
+}
+logError("[ERROR] This is an error message.");
