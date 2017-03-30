60b09fbc7eb09455d9c757aa0f4372000ab12b88
Matt Valdez <mattvaldez01@gmail.com>
2017-03-30 14:46:25 -0500

    refactored code to use construct and destruct

--- Log.php
+++ Log.php
@@ -2,21 +2,35 @@
 
 class Log {
 
-	// public $filename;
+	public $filename;
+	public $handle;
 
-	public function logMessage($logLevel, $message){
-		$handle = fopen($this->filename, 'a');
-		fwrite($handle, PHP_EOL . "$this->date $this->time [$logLevel] $message");
-		fclose($handle);
+	public function __construct($prefix = "log")
+	{
+		$this->filename = $prefix . "-" . date('Y-m-d') . ".log";
+		$this->handle = fopen($this->filename, 'a');
 	}
 
-	public function logInfo($logLevel, $message){
+	public function logMessage($logLevel, $message)
+	{
+		$timestamp = date("Y-m-d h:i:s");
+		fwrite($this->handle, PHP_EOL . "$timestamp [$logLevel] $message");
+	}
+
+	public function logInfo($logLevel, $message)
+	{
 		$this->logMessage($logLevel, $message);
 	}
 
 
-	public function logError($logLevel, $message){
+	public function logError($logLevel, $message)
+	{
 		$this->logMessage($logLevel, $message);
+	}
+
+	public function __destruct()
+	{
+		fclose($this->handle);
 	}
 
 }
--- log_test.php
+++ log_test.php
@@ -3,11 +3,6 @@
 require_once 'Log.php';
 
 $newLog = new Log();
-$date = date("Y-m-d");
-
-$newLog->filename = "log-$date.log";
-$newLog->date = date("Y-m-d");
-$newLog->time = date("h:i:s");
 
 $newLog->logInfo("INFO", "This is an info message.");
 $newLog->logError("ERROR", "This is an error message.");
