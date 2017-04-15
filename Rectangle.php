90f4d344e62a0b1c8c27fe56efcb0a015a89dd38
Matt Valdez <mattvaldez01@gmail.com>
2017-04-14 23:35:53 -0500

    wrote out Rectangle class with private and protected properties and with setters and getters

--- Rectangle.php
+++ Rectangle.php
@@ -1,4 +1,43 @@
 <?php  
+class Rectangle
+{
+	private $height;
+	private $width;
 
+	public function __construct($height, $width)
+	{
+		$this->setHeight($height);
+        $this->setWidth($width);
+	}
 
+	protected function setHeight($height)
+    {
+        $this->height = trim(htmlspecialchars(strip_tags($height)));
+    }
+
+    protected function setWidth($width)
+    {
+        $this->width = trim(htmlspecialchars(strip_tags($width)));
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+	public function area() 
+	{
+		return $this->getHeight() * $this->getWidth();
+	}
+
+	public function perimeter()
+	{
+		return ($this->getHeight() * 2) + ($this->getWidth() * 2);
+	}
+}
 ?>
