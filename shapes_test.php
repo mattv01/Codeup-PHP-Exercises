118635da0fbe75839df46670a08d8e32287fcd48
Matt Valdez <mattvaldez01@gmail.com>
2017-04-17 10:48:27 -0500

    testing visibility

--- shapes_test.php
+++ shapes_test.php
@@ -3,8 +3,8 @@ require_once __DIR__ . '/Square.php';
 require_once __DIR__ . '/Rectangle.php';
 
 $rectangle = new Rectangle(10, 20);
-$rectangle->height = 4;
-$rectangle->width = 5;
+// $rectangle->height = 4;
+// $rectangle->width = 5;
 echo $rectangle->area() . PHP_EOL;
 
 $square = new Square(10);
